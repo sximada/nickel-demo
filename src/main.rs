046ba962ffa2e8864d2b06cad30c703c2b61b2f5
sximada <50688746+sximada@users.noreply.github.com>
2019-08-07 17:55:37 +0900

    Add simple nickel server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+#[macro_use] extern crate nickel;
+
+use nickel::Nickel;
+
 fn main() {
-    println!("Hello, world!");
+    let mut server = Nickel::new();
+
+    server.utilize(router! {
+        get "**" => |_req, _res| {
+            "Hello World!!"
+        }
+    });
+    server.listen("127.0.0.1:6767").unwrap();
 }
