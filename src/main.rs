88e9b19b70247f9b13f1174a92f9359c33603d4c
sximada <50688746+sximada@users.noreply.github.com>
2019-08-07 19:31:49 +0900

    Implemented flexible routing.
    http://nickel-org.github.io/#routing

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
 #[macro_use] extern crate nickel;
 
-use nickel::Nickel;
+use nickel::{Nickel, HttpRouter};
 
 fn main() {
     let mut server = Nickel::new();
 
-    server.utilize(router! {
-        get "**" => |_req, _res| {
-            "Hello World!!"
-        }
+    server.get("/bar", middleware!("This is the /bar handler"));
+    server.get("/user/:userid", middleware!{ |request|
+        format!("This is user: {:?}", request.param("userid"))
     });
+    server.get("/a/*/d", middleware!("matches /a/b/d but not /a/b/c/d"));
+    server.get("/a/**/d", middleware!("This matches /a/b/d and also /a/b/c/d"));
     server.listen("127.0.0.1:6767").unwrap();
 }
