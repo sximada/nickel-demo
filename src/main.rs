0b07c40b4fb95bebc3dfc92c4c6a622ce78622c7
sximada <50688746+sximada@users.noreply.github.com>
2019-08-07 20:16:06 +0900

    Implemented static files server

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,7 @@
-#[macro_use] extern crate nickel;
-
-use nickel::{Nickel, HttpRouter};
+use nickel::{Nickel, StaticFilesHandler};
 
 fn main() {
     let mut server = Nickel::new();
-
-    server.get("/bar", middleware!("This is the /bar handler"));
-    server.get("/user/:userid", middleware!{ |request|
-        format!("This is user: {:?}", request.param("userid"))
-    });
-    server.get("/a/*/d", middleware!("matches /a/b/d but not /a/b/c/d"));
-    server.get("/a/**/d", middleware!("This matches /a/b/d and also /a/b/c/d"));
+    server.utilize(StaticFilesHandler::new("assets/"));
     server.listen("127.0.0.1:6767").unwrap();
 }
