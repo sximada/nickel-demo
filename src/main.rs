507e89093312b4bd35fdf60668a95da1ba463810
sximada <50688746+sximada@users.noreply.github.com>
2019-08-07 20:57:50 +0900

    Override the default error handler to handle 404 cases with a custom view

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,23 @@
 #[macro_use] extern crate nickel;
-extern crate serde;
-extern crate serde_json;
-#[macro_use] extern crate serde_derive;
 
-
-use nickel::{Nickel, HttpRouter, JsonBody};
-use serde::{Serialize, Deserialize};
-
-#[derive(Serialize, Deserialize)]
-struct Person {
-    firstname: String,
-    lastname:  String,
-}
+use std::io::Write;
+use nickel::status::StatusCode::NotFound;
+use nickel::{Nickel, NickelError, Action, Continue, Halt, Request};
 
 fn main() {
     let mut server = Nickel::new();
 
-    server.post("/a/post/request", middleware! { |request, response|
-        let person = request.json_as::<Person>().unwrap();
-        format!("Hello {} {}", person.firstname, person.lastname)
-    });
+    fn custom_404<'a>(err: &mut NickelError, _req: &mut Request) -> Action {
+        if let Some(ref mut res) = err.stream {
+            if res.status() == NotFound {
+                let _ = res.write_all(b"<p>Call the police!</p>");
+                return Halt(())
+            }
+        }
+        Continue(())
+    }
 
+    let custom_hander: fn(&mut NickelError, &mut Request) -> Action = custom_404;
+    server.handle_error(custom_hander);
     server.listen("127.0.0.1:6767");
 }
