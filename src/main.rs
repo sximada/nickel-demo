3f3502f41deea4e157a5581927dce6d5febeaa15
sximada <50688746+sximada@users.noreply.github.com>
2019-08-07 20:43:56 +0900

    json example api

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,25 @@
-use nickel::{Nickel, StaticFilesHandler};
+#[macro_use] extern crate nickel;
+extern crate serde;
+extern crate serde_json;
+#[macro_use] extern crate serde_derive;
+
+
+use nickel::{Nickel, HttpRouter, JsonBody};
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize)]
+struct Person {
+    firstname: String,
+    lastname:  String,
+}
 
 fn main() {
     let mut server = Nickel::new();
-    server.utilize(StaticFilesHandler::new("assets/"));
-    server.listen("127.0.0.1:6767").unwrap();
+
+    server.post("/a/post/request", middleware! { |request, response|
+        let person = request.json_as::<Person>().unwrap();
+        format!("Hello {} {}", person.firstname, person.lastname)
+    });
+
+    server.listen("127.0.0.1:6767");
 }
