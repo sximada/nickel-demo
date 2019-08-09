fb2925c7095b7cc68f280e29d1439097c07e78c2
sximada <50688746+sximada@users.noreply.github.com>
2019-08-09 15:44:33 +0900

    Add dotnev settings

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,18 @@
+extern crate dotenv;
+
+use std::env;
 use std::collections::HashMap;
+use dotenv::dotenv;
 use nickel::{
     HttpRouter,
     MiddlewareResult,
+    Mountable,
     Nickel,
     Request,
     Response,
     StaticFilesHandler,
 };
-use nickel::Mountable;
+
 
 fn root<'mw, 'conn>(_req: &mut Request<'mw, 'conn>, res: Response<'mw>) -> MiddlewareResult<'mw> {
     let mut data = HashMap::new();
@@ -17,9 +22,22 @@ fn root<'mw, 'conn>(_req: &mut Request<'mw, 'conn>, res: Response<'mw>) -> Middl
 
 
 fn main() {
-    let mut server = Nickel::new();
-    server.get("/", root);
-    server.mount("/static/", StaticFilesHandler::new("assets"));
-    server.listen("127.0.0.1:6767").unwrap();
-}
+    dotenv().ok();
 
+    let mut server = Nickel::new();
+    let domain = match env::var("HTTP_DOMAIN") {
+        Ok(host) => host,
+        Err(_) => "127.0.0.1:6767".to_string(),
+    };
+    let assets_path = match env::var("ASSETS_PATH") {
+        Ok(path) => path,
+        Err(_) => "assets".to_string(),
+    };
+
+    server.get("/", root);
+    server.mount("/static/", StaticFilesHandler::new(assets_path));
+    match server.listen(domain) {
+        Ok(_) => (),
+        Err(err) => println!("Failed: {}", err)
+    }
+}
